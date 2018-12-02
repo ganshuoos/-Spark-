4d4713c065318fc04847508d0c9750df2d6c0fb8
John Smith <1501681615@qq.com>
2018-12-03 01:07:04 +0800

    Commit
    一个做了封装的简单爬虫，爬取证券之星网站上的股票数据。

---
+++ -Spark-/Gecko/ZhihuDemo/ZhihuTry.py
@@ -0,0 +1,77 @@
+import urllib.request
+import re
+
+url = 'http://daily.zhihu.com/'
+
+
+def get_html(url):
+    html = urllib.request.urlopen(url).read().decode('utf-8')
+    return html
+
+
+def get_url_num(html):
+    res = re.compile('<a href="/story/(.*?)"')
+    url_nums = re.findall(res, html)
+    urls = []
+    for url_num in url_nums:
+        urls.append('http://daily.zhihu.com/story/' + url_num)
+    return urls
+
+
+def get_content(newurl):
+    newurl_html = get_html(newurl)
+    re_title = re.compile('<title>(.*?)</title>')
+    title = re.findall(re_title, newurl_html)
+    re_content = re.compile('<div class="content">\\n<p>(.*?)</div>', re.S)
+    contents = re.findall(re_content, newurl_html)
+    print(title)
+    for content in contents:
+        content = content.replace('<p>', '')
+        content = content.replace('</p>', '')
+        content = content.replace('<strong>', '')
+        print(content)
+
+
+result = get_url_num(get_html(url))
+
+print("结果为：" + str(result))
+
+for newurl in result:
+    get_content(newurl)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
